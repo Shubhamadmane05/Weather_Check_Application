95af985351fbe62f06cf7e618500d367610b3b3a
Shubham Admane <110490894+Shubhamadmane05@users.noreply.github.com>
2025-11-26 21:55:46 +0530

    Update SearchWeather.jsx

--- weather-ui/src/components/SearchWeather.jsx
+++ weather-ui/src/components/SearchWeather.jsx
@@ -14,7 +14,7 @@ const SearchWeather = () => {
     }
 
     try {
-      const API_KEY = "API KEY";
+      // const API_KEY = "API KEY";
 
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city.trim()}&appid=${API_KEY}&units=metric`
